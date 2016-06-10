dba4fd3959b02a6fe2c48725ce42d75ea3a9004f
shmakova@vl.ru <shmakova@vl.ru>
2016-06-10 16:41:33 +0600

    add timer

--- timer/js/timer.js
+++ timer/js/timer.js
@@ -1,14 +1,12 @@
+var intervalCount = 1;
+
 function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
     var seconds = Math.floor((t / 1000) % 60);
     var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
     
     return {
         'total': t,
-        'days': days,
-        'hours': hours,
         'minutes': minutes,
         'seconds': seconds
     };
@@ -16,16 +14,12 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
     var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
     var minutesSpan = clock.querySelector('.minutes');
     var secondsSpan = clock.querySelector('.seconds');
 
     function updateClock() {
         var t = getTimeRemaining(endtime);
 
-        daysSpan.innerHTML = t.days;
-        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
@@ -38,5 +32,42 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+function getEndtime(minutes, seconds) {
+    return new Date(Date.parse(new Date()) + (minutes * 60 + seconds) * 1000);
+}
+
+
+
+$('#start').on('click', function(event) {
+    event.preventDefault();
+    
+    for (i = 0; i < intervalCount; i++) {
+        var minutes = 0;
+        var seconds = 10;
+        beep(); 
+        
+        (function() {
+            setTimeout(function() {
+                var deadline = getEndtime(minutes, seconds);
+                console.log(seconds);
+                initializeClock('clock', deadline);
+            }, (minutes * 60 + seconds) * 1000);  
+        });
+    }
+    
+});
+
+$('#add').on('click', function(event) {
+    event.preventDefault();
+    var $newRow = $(".interval-row").eq(intervalCount - 1).clone(true);
+    intervalCount++;
+    $newRow.find(".interval-number").html(intervalCount + ".");
+    var $curRow = $(this).closest('tr');
+    $curRow.before($newRow);
+});
+
+function beep() {
+    var sound = document.getElementById("audio");
+    sound.play()
+}
+
